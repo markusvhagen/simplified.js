9e27f6178ab4b3aec6d1295806d43298bc62ecaa
markusvhagen <markusvhagen@gmail.com>
2017-04-24 20:14:23 +0200

    getting id or other elements, hide/show function

--- src/simplified.js
+++ src/simplified.js
@@ -1,4 +1,39 @@
-function test() {
-    var testMessage = "Hallo from simplified.js";
-    return(testMessage);
+"use strict";
+
+/* All functions have "_" in front of it. This is to prevent it from
+overwriting any other functions in another file */
+
+/* ==== SELECTORS ==== */
+
+// For divs only
+function _getId(el) {
+    return (document.getElementById(el));
+}
+
+// For all elements; class, div, name etc.
+function _getElements(el) {
+    return (document.querySelector(el));
+}
+
+
+/* ==== CSS ==== */
+
+function _show(el) {
+    if (el.style.display == "block") {
+        return false;
+    }
+    else {
+        el.style.display = "block";
+        return true;
+    }
+}
+
+function _hide(el) {
+    if (el.style.display == "none") {
+        return false;
+    }
+    else {
+        el.style.display = "none";
+        return true;
+    }
 }
--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,16 @@
-window.onload = function() {
-    console.log(test());
+var test = _getId("test");
+var hidebtn = _getId("hide");
+var showbtn = _getId("show");
+var hidebtn = document.getElementById("hide");
+var showbtn = document.getElementById("show");
+
+hidebtn.onclick = function hide() {
+    _hide(test);
+    // test.style.display = "none";
+    console.log("Kjører hide");
+}
+
+showbtn.onclick = function show() {
+    _show(test);
+    console.log("Kjører show");
 }
